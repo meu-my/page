17b19e9375ae2893ccb16c92767091cd4ced7816
meu-my <119830387+meu-my@users.noreply.github.com>
2023-06-05 01:04:58 +0900

    Add files via upload

--- header.js
+++ header.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // ヘッダーボタンを作成
     function createHeaderButton(text, page) {
-        var currentRootPath = window.location.origin;
+        var currentRootPath = "https://meu-my.github.io/page/"
         //var currentRootPath = "file:///M:/web/";
         var path = currentRootPath+page+".html"
         var button = document.createElement('button');
