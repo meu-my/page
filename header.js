fd973b35a2f7fb894c188c34e519e3cfbe7a9ef7
meu-my <119830387+meu-my@users.noreply.github.com>
2023-06-05 01:13:04 +0900

    Update header.js

--- header.js
+++ header.js
@@ -25,6 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function createHeaderButton(text, page) {
         var currentRootPath = "https://meu-my.github.io/page/"
         //var currentRootPath = "file:///M:/web/";
+        console.log(currentRootPath)
         var path = currentRootPath+page+".html"
         var button = document.createElement('button');
         button.className = 'header-button';
